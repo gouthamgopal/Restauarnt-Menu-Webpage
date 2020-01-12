e95b8b77f7a9cb55e7b9acb989e44c178e1e23a2
Goutham Gopal <goutham.gopal94@gmail.com>
2020-01-12 02:20:14 -0500

    new templates with styles

--- vagrant/project.py
+++ vagrant/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -27,6 +27,7 @@ def newMenuItem(restaurant_id):
         menuItem = MenuItem(name=request.form['name'], price=request.form['price'], description=request.form['description'], restaurant_id=restaurant_id)
         session.add(menuItem)
         session.commit()
+        flash("new menu item created!")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template('newMenuItem.html', restaurant_id=restaurant_id)
@@ -41,16 +42,25 @@ def editMenuItem(restaurant_id, menu_id):
         item.name = request.form['name']
         session.add(item)
         session.commit()
+        flash(item.name + " menu item was edited!")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
-        return render_template('editmenuitem.html', restaurant_id=restaurant_id, menu_id= menu_id, menu_name=item.name)
+        return render_template('editmenuitem.html', restaurant_id=restaurant_id, menu_id= menu_id, item=item)
 
 # Task 3: Create a route for deleteMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete', methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-    return "page to delete a menu item. Task 3 complete!"
+    item = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        session.delete(item)
+        session.commit()
+        flash(item.name + " menu item was deleted!")
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('deletemenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=item)
 
 if __name__ == '__main__':
+    app.secret_key = "something_secret"
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
